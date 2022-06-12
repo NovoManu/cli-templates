6f1a5ac653900b569717e8321417deaade77c5b5
Manu <ustenko.mi@gmail.com>
2022-06-12 17:26:50 +0200

    Add router into vue3

--- templates/vue3/settings.js
+++ templates/vue3/settings.js
@@ -3,10 +3,12 @@ const getInstallationSettings =
     const settings = {
       templates: {
 				base: true,
-	      vitest: false
+	      vitest: false,
+	      router: false
       }
     }
     settings.templates.vitest = await confirm({ message: 'Do you want to include vitest?' })
+    settings.templates.router = await confirm({ message: 'Do you want to include vue router?' })
     return settings
   }
 	
