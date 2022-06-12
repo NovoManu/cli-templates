d054c2cb4ae80446b17addc4a4dba051283c7cc0
Manu <ustenko.mi@gmail.com>
2022-06-12 17:40:35 +0200

    Add dev port config into vue3

--- templates/vue3/settings.js
+++ templates/vue3/settings.js
@@ -1,12 +1,28 @@
 const getInstallationSettings =
-  async ({ confirm }) => {
+  async ({ confirm, input }) => {
     const settings = {
+			devPort: 8080,
       templates: {
 				base: true,
 	      vitest: false,
 	      router: false
       }
     }
+		  const devPort = await input({
+	    message: 'Enter development port',
+	    default: 8080,
+	    validate: (value) =>
+	      new Promise((resolve) => {
+	        setTimeout(
+	          () =>
+	            resolve(
+	              Number.isInteger(Number(value)) || 'You must provide a number'
+	            ),
+	          3000
+	        )
+	      })
+	  })
+	  settings.devPort = Number(devPort)
     settings.templates.vitest = await confirm({ message: 'Do you want to include vitest?' })
     settings.templates.router = await confirm({ message: 'Do you want to include vue router?' })
     return settings
