cb6d402a8770a0f1b6ae8fd4ffbeab6e2df1b190
Manu Ustenko <ustenko.mi@gmail.com>
2022-07-10 21:16:12 +0200

    Fix test composable template settings

--- templates/test-composable/[[b]]settings.js
+++ templates/test-composable/[[b]]settings.js
@@ -8,23 +8,15 @@ const getInstallationSettings =
 	      router: false
       }
     }
-		  const devPort = await input({
-	    message: 'Enter development port',
-	    default: 8080,
-	    validate: (value) =>
-	      new Promise((resolve) => {
-	        setTimeout(
-	          () =>
-	            resolve(
-	              Number.isInteger(Number(value)) || 'You must provide a number'
-	            ),
-	          3000
-	        )
-	      })
-	  })
-	  settings.devPort = Number(devPort)
-    settings.modules.vitest = await confirm({ message: 'Do you want to include vitest?' })
-    settings.modules.router = await confirm({ message: 'Do you want to include vue router?' })
+    const devPort = await input(
+      'Enter development port',
+      '8080',
+      null,
+      (value) => Number.isInteger(Number(value)) || 'You must provide a number'
+    )
+    settings.dev_port = Number(devPort)
+    settings.modules.vitest = await confirm('Do you want to include vitest?')
+    settings.modules.router = await confirm('Do you want to include vue router?')
 	  if (settings.modules.router) {
 			settings.routerImport = 'import router from "./router";'
 		  settings.routerUse = 'app.use(router);'
