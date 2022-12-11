12fb510b61a0cb4a7da0d51714755422905e1cf6
Manu Ustenko <ustenko.mi@gmail.com>
2022-12-11 11:01:31 +0100

    Update vite config

--- templates/html-academy-vue3-pizza/frontend/vite.config.js
+++ templates/html-academy-vue3-pizza/frontend/vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+	test: {
+		environment: 'jsdom',
+	},
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -13,6 +16,12 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 8080
+    port: 8080,
+	  proxy: {
+      "/api": {
+        target: "http://backend:3000/",
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   }
 })
